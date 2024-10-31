8a86250ddd7d98cb850de31cd9beba4668428a27
HEXATRIA <hexaguma@gmail.com>
2024-10-31 12:28:14 +0800

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,6 @@
+# Tugas lalu putra satria wijaya
+print("nama saya adalah[lalu putra satria wijaya]")
+print("nim saya adalah [24241139]")
+print("saya berasal dari[MOJOKERTO,Jawatimur]")
+print("nomer keberuntungan saya adalah : 2439")
+print("TERIMAKASIH")
