cca8af4ce08290c47f5958a4222a37e5afd39637
HEXATRIA <hexaguma@gmail.com>
2024-11-10 11:46:58 +0800

    Add files via upload
    TUGAS 4, Program Flowchart

---
+++ tugas 4.py
@@ -0,0 +1,14 @@
+#hitunglah luas segitiga
+a = 39
+t = 24
+#rumus mencari luas segitiga
+luas = 1/2*a*t
+
+print("nilai a adalah : ", a)
+print("nilai t adalah : ", t)
+print("nilai luas adalah : ", luas )
+
+#hasil dari hitung luas segitiga
+a = 39
+t = 24
+luas = 468.0
